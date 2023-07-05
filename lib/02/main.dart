25c0073ffdbff80d3da6073402413c480d8c664a
anphu123 <anphu221002@gmail.com>
2023-07-06 03:39:20 +0700

    a

--- lib/02/main.dart
+++ lib/02/main.dart
@@ -80,62 +80,106 @@ class MyHomePage extends StatefulWidget {
 //     );
 //   }
 // }
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//         backgroundColor: Colors.black,
+//       ),
+//       body: SafeArea(
+//         child: Container(
+//           padding: EdgeInsets.all(16),
+//           color: Colors.brown,
+//           child: ListView.builder(
+//             itemCount: Recipe.samples.length,
+//             itemBuilder: (BuildContext context, int index) {
+//               return Card(
+//                 elevation: 2,
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(16),
+//                 ),
+//                 child: ClipRRect(
+//                   borderRadius: BorderRadius.circular(12),
+//                   child: Stack(
+//                     children: [
+//                       Image.asset(
+//                         Recipe.samples[index].hinh,
+//                         fit: BoxFit.cover,
+//                         width: double.infinity,
+//                         height: 200,
+//                       ),
+//                       Positioned(
+//                         bottom: 0,
+//                         left: 0,
+//                         right: 0,
+//                         child: Container(
+//                           padding: EdgeInsets.all(8),
+//                           color: Colors.black.withOpacity(0.6),
+//                           child: Text(
+//                             Recipe.samples[index].ten,
+//                             style: TextStyle(
+//                               color: Colors.white,
+//                               fontSize: 20,
+//                               fontWeight: FontWeight.bold,
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        backgroundColor: Colors.black,
       ),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(16),
-          color: Colors.brown,
-          child: ListView.builder(
+        child: ListView.builder(
             itemCount: Recipe.samples.length,
             itemBuilder: (BuildContext context, int index) {
-              return Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Stack(
-                    children: [
-                      Image.asset(
-                        Recipe.samples[index].hinh,
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                        height: 200,
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          color: Colors.black.withOpacity(0.6),
-                          child: Text(
-                            Recipe.samples[index].ten,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
+              return buildRecipeCard(Recipe.samples[index]);
+            }),
       ),
     );
   }
 }
 
+Widget buildRecipeCard(Recipe recipe) {
+  // 1
+  return Card(
+    elevation: 2.0,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    // 2
+    child: Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        // 3
+        children: <Widget>[
+          // 4
+          Image(image: AssetImage(recipe.hinh)),
+          // 5
+          Text(
+            recipe.ten,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Arial',
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
