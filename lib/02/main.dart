b40dafdaf0ee8a492bfedbbdbe880d7817799fdd
anphu123 <anphu221002@gmail.com>
2023-07-06 05:46:52 +0700

    0

--- lib/02/main.dart
+++ lib/02/main.dart
@@ -1,15 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled2/02/Recipe.dart';
-import 'Recipe.dart';
+import 'recipe_detail.dart';
 import 'Recipe.dart';
 
 void main() {
-  runApp(RecipeApp());
+  runApp(const RecipeApp());
 }
 
 class RecipeApp extends StatelessWidget {
-  const RecipeApp({super.key});
+  const RecipeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +27,7 @@ class RecipeApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -37,107 +35,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: SafeArea(
-//         child: Container(
-//           color: Colors.blueGrey,
-//           child: ListView.builder(
-//             // 5
-//             itemCount: Recipe.samples.length,
-//             // 6
-//             itemBuilder: (BuildContext context, int index) {
-//               // 7
-//               // TODO: Update to return Recipe card
-//               return Container(
-//                 width: 200,
-//                 height: 200,
-//                 padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(50),
-//                   boxShadow: [
-//                     BoxShadow(
-//                       color: Colors.black.withOpacity(0.2),
-//                       offset: Offset(2, 2),
-//                       blurRadius: 5,
-//                     ),
-//                   ],
-//                 ),
-//                 child: Image.asset(
-//                   Recipe.samples[index].hinh,
-//                   fit: BoxFit.cover,
-//                 ),
-//               );
-//             },
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//         backgroundColor: Colors.black,
-//       ),
-//       body: SafeArea(
-//         child: Container(
-//           padding: EdgeInsets.all(16),
-//           color: Colors.brown,
-//           child: ListView.builder(
-//             itemCount: Recipe.samples.length,
-//             itemBuilder: (BuildContext context, int index) {
-//               return Card(
-//                 elevation: 2,
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(16),
-//                 ),
-//                 child: ClipRRect(
-//                   borderRadius: BorderRadius.circular(12),
-//                   child: Stack(
-//                     children: [
-//                       Image.asset(
-//                         Recipe.samples[index].hinh,
-//                         fit: BoxFit.cover,
-//                         width: double.infinity,
-//                         height: 200,
-//                       ),
-//                       Positioned(
-//                         bottom: 0,
-//                         left: 0,
-//                         right: 0,
-//                         child: Container(
-//                           padding: EdgeInsets.all(8),
-//                           color: Colors.black.withOpacity(0.6),
-//                           child: Text(
-//                             Recipe.samples[index].ten,
-//                             style: TextStyle(
-//                               color: Colors.white,
-//                               fontSize: 20,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               );
-//             },
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -147,39 +44,57 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: SafeArea(
         child: ListView.builder(
-            itemCount: Recipe.samples.length,
-            itemBuilder: (BuildContext context, int index) {
-              return buildRecipeCard(Recipe.samples[index]);
-            }),
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // TODO: Replace return with return RecipeDetail()
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
+        ),
       ),
     );
   }
-}
 
-Widget buildRecipeCard(Recipe recipe) {
-  // 1
-  return Card(
-    elevation: 2.0,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-    // 2
-    child: Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        // 3
-        children: <Widget>[
-          // 4
-          Image(image: AssetImage(recipe.hinh)),
-          // 5
-          Text(
-            recipe.ten,
-            style: const TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.w700,
-              fontFamily: 'Arial',
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      // 1
+      elevation: 2.0,
+      // 2
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      // 3
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 4
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.hinh)),
+            // 5
+            const SizedBox(
+              height: 14.0,
             ),
-          ),
-        ],
+            // 6
+            Text(
+              recipe.ten,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
---
+++ lib/02/recipe_detail.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+
+  const RecipeDetail({
+    Key? key,
+    required this.recipe,
+  }) : super(key: key);
+
+  @override
+  State<RecipeDetail> createState() {
+    return _RecipeDetailState();
+  }
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  // TODO: Add _sliderVal here
+
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.ten),
+      ),
+      // 2
+      body: SafeArea(
+        // 3
+        child: Column(
+          children: <Widget>[
+            // 4
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image(
+                image: AssetImage(widget.recipe.hinh),
+              ),
+            ),
+            // 5
+            const SizedBox(
+              height: 4,
+            ),
+            // 6
+            Text(
+              widget.recipe.ten,
+              style: const TextStyle(fontSize: 18),
+            ),
+            // TODO: Add Expanded
+
+            // TODO: Add Slider() here
+          ],
+        ),
+      ),
+    );
+  }
+}
