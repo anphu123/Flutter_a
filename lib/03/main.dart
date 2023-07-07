0f38218009d83836e77e8559149914bc37dd2f03
anphu123 <anphu221002@gmail.com>
2023-07-07 17:08:14 +0700

    03

---
+++ lib/03/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(BcApp());
+}
+class BcApp extends StatelessWidget {
+  const BcApp8({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
