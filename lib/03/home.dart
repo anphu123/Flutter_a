a4787a48222f5570b23e5c82eeae72fdcdc70a24
anphu123 <anphu221002@gmail.com>
2023-07-08 05:48:22 +0700

    03

---
+++ lib/03/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'card1.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // TODO: Add state variables and functions
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    // TODO: Replace with Card1
+    const Card1(),
+    // TODO: Replace with Card2
+    Container(color: Colors.green),
+    // TODO: Replace with Card3
+    Container(color: Colors.blue)
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich home',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      // TODO: Show selected tab
+      body: pages[_selectedIndex],
+      // TODO: Add bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // TODO: Set selected tab bar
+        // 10
+        currentIndex: _selectedIndex,
+// 11
+        onTap: _onItemTapped,
+
+        //todo: khi sử dụng bottom navigation bar phải có tối thiểu 2 item
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard), //todo: add icon
+            label: 'Card2', //todo: add label
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/03/main.dart
+++ lib/03/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
+import 'home.dart';
+
 
 void main() {
   runApp(const Fooderlich());
@@ -18,22 +20,7 @@ class Fooderlich extends StatelessWidget {
       // TODO: Add theme
       theme: theme,
       title: 'Fooderlich',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            style: theme.textTheme.headline6,
-          ),
-        ),
-        body: Center(
-          // TODO: Style the title
-          child: Text(
-            'Let\'s get cooking 👩‍🍳',
-            // TODO: Style the body text
-            style: theme.textTheme.headline1,
-          ),
-        ),
-      ),
+      home: Home(),
     );
   }
 }
