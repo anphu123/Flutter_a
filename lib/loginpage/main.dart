03ef3e59200d797465b23f090d3217fb0685ce13
anphu123 <anphu221002@gmail.com>
2023-08-23 03:14:41 +0700

    a

--- lib/loginpage/main.dart
+++ lib/loginpage/main.dart
@@ -17,6 +17,61 @@ class HomeScreen extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.blue,
+        child: Center(
+          child: Container(
+            width: 400,
+            height: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 300,
+                  child: Material(
+                    elevation: 5,
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                        ), //tạo icon
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Expanded(
+                          child: TextField(
+                            // TODO  tạo edittext
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(10),
+                                        bottomRight: Radius.circular(10))),
+                                fillColor: Colors.white,
+                                filled: true,
+                                hintText: "User name",
+                                hintStyle: TextStyle(
+                                  fontSize: 15,
+                                )),
+                            cursorColor: Colors.red,
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
